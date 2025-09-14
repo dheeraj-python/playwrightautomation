80f50bf745ba2d5cd0e0d7e30fe884463a545861
DheerajPC <dheerajofficial27@gmail.com>
2025-09-14 20:15:30 +0530

    fixed the issue of file path

--- util/utils/test_user_registration.py
+++ util/utils/test_user_registration.py
@@ -1,7 +1,7 @@
 import time
 
 from playwright.sync_api import Page, Playwright, expect
-
+#from util.utils import credential
 
 def test_login_functionality(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -12,12 +12,12 @@ def test_login_functionality(playwright):
 
 def test_registration_page(page: Page):
     page.goto("https://awesomeqa.com/ui/index.php?route=account/register")
-    page.get_by_label("First Name").fill("Dheeraj")
-    page.get_by_label("Last Name").fill("Sharma")
-    page.get_by_label("E-Mail").fill("drj1901@gmail.com")
-    page.get_by_label("Telephone").fill("7007183451")
-    page.get_by_role("textbox", name="* Password", exact=True).fill("Nrjshrm")
-    page.get_by_role("textbox", name="* Password Confirm", exact=True).fill("Nrjshrm")
+    page.get_by_label("First Name").fill("DheerajS")
+    page.get_by_label("Last Name").fill("SharmaS")
+    page.get_by_label("E-Mail").fill("drj19001@gmail.com")
+    page.get_by_label("Telephone").fill("7007183401")
+    page.get_by_role("textbox", name="* Password", exact=True).fill("Nrjshrm12")
+    page.get_by_role("textbox", name="* Password Confirm", exact=True).fill("Nrjshrm12")
     page.get_by_role("checkbox").check()
     page.get_by_role("button", name="Continue").click()
     #check for the below text should be visible after clicking on Continue button
